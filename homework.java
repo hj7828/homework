a2234f1fdf169146a3cf0b993713e56146f04c6b
9527828 <9527828@naver.com>
2017-04-18 14:58:29 +0900

    First commit

---
+++ homework.java
@@ -0,0 +1,7 @@
+public class homework {
+  public static void main(String[] arge) {
+    System.out.println("Hello, world from GIT");
+    System.out.println("Hello, D Commit");
+    System.out.println("Hello, C Commit");
+  }
+}
